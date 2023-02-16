aecd7438b985ee04be33a832e43d9ea0e0025445
dhairyaaaaa14 <dhairyapan15@gmail.com>
2023-02-16 15:04:38 +0530

    Final commit Co-authored-by: Ap0000rv <Ap0000rv@users.noreply.github.com>

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useNavigate } from "react";
 import Input from "./Input";
 import api from "../api/data";
 import { v1 as uuid } from "uuid";
+import "./css/FormInput.css"
 import {
   BrowserRouter as Router,
   Routes,
@@ -9,17 +10,9 @@ import {
   Redirect,
   Link,
 } from "react-router-dom";
-import ShowButton from "./ShowButton";
-
-
-
-
-
+  
 
 const FormInput = () => {
-    const [isChecked, setIsChecked] = useState(false);
-    const [checkedName, setCheckedName] = useState("");
-    const[hobby,setHobby] = useState("Cricket");
 
   const [formData, setFormData] = useState({
     inputText: "",
@@ -27,7 +20,7 @@ const FormInput = () => {
     inputPassword: "",
     inputCheckbox: [],
     inputRadio: "",
-    inputSelect: "",
+    inputSelect: "Cricket",
     inputTextArea: "",
   });
 
@@ -39,35 +32,50 @@ const FormInput = () => {
     };
     const response = await api.post("/data", request);
     console.log(response);
-    setFormData(response.data);
+    // setFormData(response.data);
+    setFormData({
+      inputText: "",
+      inputEmail: "",
+      inputPassword: "",
+      inputCheckbox: [],
+      inputRadio: "",
+      inputSelect: "",
+      inputTextArea: "",
+    });
   };
+  const inputCheckboxList = [
+    "react","nodejs","java"
+  ]
 
-  const handleChange = (e) => {
-   
-    const { name, value, type, checked } = e.target;
-    setIsChecked(e.target.checked);
-   
-    if (type==="checkbox" && e.target.checked) {
-      setCheckedName(e.target.name);
-      setFormData({
-        ...formData,
-        inputCheckbox:[...formData.inputCheckbox,e.target.name], 
-      })
-      console.log(e.target.name);
+
+  const handleCheckboxChange = (event) => {
+    const checkboxName = event.target.name;
+    const checkboxValue = event.target.checked;
+
+    const updatedinputCheckbox = formData.inputCheckbox.slice(); // make a copy of the interests array
+    console.log(updatedinputCheckbox)
+    if (checkboxValue) {
+      updatedinputCheckbox.push(checkboxName);
+    console.log(updatedinputCheckbox)
+
     } else {
-        setCheckedName("");
+      const index = updatedinputCheckbox.indexOf(checkboxName);
+      updatedinputCheckbox.splice(index, 1);
+    console.log(updatedinputCheckbox)
+
     }
 
-     if(type === "select-one")setHobby(value);
+    setFormData({ ...formData, inputCheckbox: updatedinputCheckbox });
+  }
 
-    
-    // console.log(value);
-    // console.log(e.target.value);
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
     setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: type === "checkbox" ? checked : value,
     }));
-    console.log(value);
+
   };
 
   function handleInput(e) {
@@ -75,142 +83,231 @@ const FormInput = () => {
       formData.inputText === "" ||
       formData.inputEmail === "" ||
       formData.inputTextArea === "" ||
-      formData.inputSelect === ""
+      formData.inputSelect === "" || formData.inputRadio === ""
     ) {
       alert("Please complete the form");
       e.preventDefault();
     }
 
-    if( formData.inputCheckbox.length===0 ){
-        alert("Select tech")
-        e.preventDefault();
+    if (formData.inputCheckbox.length === 0) {
+      alert("Select tech");
+      e.preventDefault();
     }
+
+    
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
     handleInput();
     addData();
+
+    console.log(formData);
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div class="justify-content-center text-center">
+      <form class="container" onSubmit={handleSubmit}>
         <h1 style={{ textAlign: "center" }}>Form Page</h1>
-        <div className="container">
-          <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Name</label>
-            <input onChange={handleChange} type="text" class="form-control" id="exampleFormControlInput1" placeholder="" />
-        </div>
-          <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Email address</label>
-            <input onChange={handleChange} type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-        </div>
-          <br />
-          <label>
-            Password
-            <Input
-              type="password"
-              name="inputPassword"
-              value={formData.inputPassword}
-              onChange={handleChange}
-            />
-          </label>
-          <br />
-          <label>Your preferred teck Stack</label>
-          <br></br>
+        <div className="container col">
+          <div className="row">
+            <div class="mb-3 col-6">
+              <label for="exampleFormControlInput1" class="form-label">
+                Name
+              </label>
+              <input
+                name="inputText"
+                onChange={handleChange}
+                type="text"
+                class="form-control"
+                id="exampleFormControlInput1"
+                placeholder=""
+                value={formData.inputText}
+              />
+            </div>
+            <div class="mb-3 col-6">
+              <label for="exampleFormControlInput1" class="form-label">
+                Email address
+              </label>
+              <input
+                name="inputEmail"
+                onChange={handleChange}
+                type="email"
+                class="form-control"
+                id="exampleFormControlInput1"
+                placeholder="name@example.com"
+                value={formData.inputEmail}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div class="mb-3 col-6">
+              <label for="exampleFormControlInput1" class="form-label">
+                Password
+              </label>
+              <input
+                onChange={handleChange}
+                name="inputPassword"
+                type="password"
+                class="form-control"
+                id="exampleFormControlInput1"
+                placeholder="Enter your password"
+                value={formData.inputPassword}
+              />
+            </div>
+          </div>
 
-          <div className="checkBoxDiv">
-            <label>React</label>
-            <Input 
+          <div className="form-check form-check-inline">
+            Your preferred teck Stack:
+          </div>
+
+          {inputCheckboxList.map((data)=>{
+            return  <div class="form-check form-check-inline">
+            <Input
+              id="check2"
+              class="form-check-input"
+              type="checkbox"
+              name={data}
+              checked={formData.inputCheckbox.includes(data)}
+              onChange={handleCheckboxChange}
+              value={data}
+            />
+            <label class="form-check-label" for="inlineCheckbox1">
+              {data}
+            </label>
+          </div>
+          })}
+
+          {/* <div className="form-check form-check-inline">
+            <Input
+              class="form-check-input"
+              id="check1"
               type="checkbox"
               name="react"
-            checked={isChecked}
+              checked={isChecked}
               onChange={handleChange}
               value="react"
             />
-
-            <label>
-              NodeJS
-              <Input
-                type="checkbox"
-                name="nodejs"
-                checked={isChecked}
-                onChange={handleChange}
-                value="nodejs"
-              />
-            </label>
-
-            <label>
-            Java
-              <Input
-                type="checkbox"
-                name="java"
-                checked={isChecked}
-                onChange={handleChange}
-                value="java"
-              />
+            <label class="form-check-label" for="inlineCheckbox1">
+              React
             </label>
           </div>
 
-          <br />
-          <label>
-            Radio Input 1:
+          <div className="form-check form-check-inline">
             <Input
-              type="radio"
-              name="inputRadio"
-              value="option1"
-              checked={formData.inputRadio === "option1"}
+              class="form-check-input"
+              type="checkbox"
+              name="nodejs"
+              checked={isChecked}
               onChange={handleChange}
+              value="nodejs"
             />
-          </label>
-          <br />
-          <label>
-            Radio Input 2:
-            <Input
-              type="radio"
-              name="inputRadio"
-              value="option2"
-              checked={formData.inputRadio === "option2"}
-              onChange={handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            Select Input:
-            <select
-              name="inputSelect"
-              value="sd"
-              onChange={handleChange}
-            >
-              <option value="-1">Select an option</option>
-              <option value="cricket">Cricket</option>
-              <option value="music">Music</option>
-              <option value="gaming">Gaming</option>
-            </select>
+            <label class="form-check-label" for="inlineCheckbox1">
+              Node
+            </label>
+          </div>
 
-          </label>
-        
-          <select class="form-select" style={{display: "block"}} aria-label="Default select example" onChange={handleChange}>
-            <option value="Cricket" selected>Cricket</option>
-            <option value="Music">Music</option>
-            <option value="Gaming">Gaming</option>
-            </select>
+          <div class="form-check form-check-inline">
+            <Input
+              id="check2"
+              class="form-check-input"
+              type="checkbox"
+              name="java"
+              checked={isChecked}
+              onChange={handleChange}
+              value="java"
+            />
+            <label class="form-check-label" for="inlineCheckbox1">
+              Java
+            </label>
+          </div> */}
+          <br /> <br/>
+          <div className="form-check form-check-inline">
+            Gender:
+          </div>
+          <div class="form-check form-check-inline">
+            <input
+              class="form-check-input"
+              type="radio"
+              name="inputRadio"
+              value="Male"
+              checked={formData.inputRadio === "Male"}
+              onChange={handleChange}
+            />
+            <label class="form-check-label" for="inlineRadio1">
+              Male
+            </label>
+          </div>
+
+          <div class="form-check form-check-inline">
+            <input
+              class="form-check-input"
+              type="radio"
+                  name="inputRadio"
+                  value="Female"
+                  checked={formData.inputRadio === "Female"}
+                  onChange={handleChange}
+            />
+            <label class="form-check-label" for="inlineRadio1">
+              Female
+            </label>
+          </div>
+
+          <div class="form-check form-check-inline">
+            <input
+              class="form-check-input"
+              type="radio"
+                  name="inputRadio"
+                  value="Other"
+                  checked={formData.inputRadio === "Other"}
+                  onChange={handleChange}
+            />
+            <label class="form-check-label" for="inlineRadio1">
+              Others
+            </label>
+          </div>
+
+          <br/><br/>
+
+          <div class="dropdown col-6">Hobbies</div>
+          <select
+            class="form-select"
+            name="inputSelect"
+            style={{ display: "block" }}
+            aria-label="Default select example"
+            onChange={handleChange}
+          >
+            <option name="inputSelect" value="Cricket" selected>
+              Cricket
+            </option>
+            <option name="inputSelect" value="Music">
+              Music
+            </option>
+            <option name="inputSelect" value="Gaming">
+              Gaming
+            </option>
+          </select>
           <br />
           <label>
-            Textarea Input:
             <textarea
+              rows="3"
+              cols="50"
+              placeholder="Add a comment"
               name="inputTextArea"
               value={formData.inputTextArea}
               onChange={handleChange}
             />
           </label>
           <br />
-          <button className="button" type="submit">
+          <button className="btn btn-primary mx-5 col-4" type="submit">
             Submit
           </button>
         </div>
       </form>
-      <Link to="/showData">Show Data</Link>
+      <br></br>
+      <Link to="/">
+    
+        <button  className="btn btn-secondary mx-5 col-6" >Show Data</button>
+      </Link>
     </div>
   );
 };
